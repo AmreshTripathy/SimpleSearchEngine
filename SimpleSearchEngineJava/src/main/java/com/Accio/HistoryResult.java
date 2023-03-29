866d4369c47751a8924718f89f18ca0a66dec468
AmreshTripathy <amreshtripathy184@gmail.com>
2023-03-29 18:11:39 +0530

    SimpleSearchEngineJava Project

---
+++ SimpleSearchEngineJava/src/main/java/com/Accio/HistoryResult.java
@@ -0,0 +1,24 @@
+package com.Accio;
+/*
+* @Amresh Tripathy
+*/
+public class HistoryResult {
+    String Keyword;
+    String Link;
+
+    public String getKeyword() {
+        return Keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        Keyword = keyword;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+}
